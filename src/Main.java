10ea227fc666d3d98be986ad3d430b522cac085c
STARTRACEX <startraceebr@foxmail.com>
2023-04-13 16:31:59 +0800

    modify Main

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        Request req = new Request("POST", "cors", true, null, null, null);
+        Request req = new Request("GET", "cors", true, null, null, null);
         Fetch f = new Fetch("http://localhost:8000/",req);
         f.Then((res) -> {
-            System.out.println("then1"+res);
-            return true;
+            System.out.println("then1  "+res);
+            return "1";
+        });
+        f.Then((res) -> {
+            System.out.println("then2  "+res);
+            return "2";
         });
         f.Catch((res) -> {
-            System.out.println("then2"+res);
-            return true;
+            System.out.println("catch  "+res);
+            return "3";
         });
         f.Finally((res) -> {
-            System.out.println("finally"+res);
-            return true;
+            System.out.println("finally  "+res);
+            return "4";
         });
     }
 }
