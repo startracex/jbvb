0a7bf27fea929da1beb05350efa149e6ca456fcc
STARTRACEX <startraceebr@foxmail.com>
2023-05-12 10:56:22 +0800

    stream

---
+++ src/stream.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class stream {
+    public static void main(String[] args) {
+        int n;
+        byte[] a = new byte[100];
+        try {
+            File f = new File("jbvb.iml");
+            InputStream in = new FileInputStream(f);
+            while ((n = in.read(a, 0, 100)) != -1) {
+                String s = new String(a, 0, n);
+                System.out.println(s);
+            }
+
+        } catch (IOException e) {
+            System.out.println("exception");
+        }
+    }
+}
