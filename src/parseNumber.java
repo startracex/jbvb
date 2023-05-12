dd5389e3f166e8a539837375ddb7a367a34262c5
STARTRACEX <startraceebr@foxmail.com>
2023-05-12 10:38:41 +0800

    update

--- src/parseNumber.java
+++ src/parseNumber.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class parseNumber {
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class parseNumber {
+    public static void main(String[] args) {
+
+        String str = "数学87分，物理76分，英语96分";
+        Scanner sc = new Scanner(str);
+        sc.useDelimiter("[^0-9]+");
+        int sum = 0;
+        double index = 0;
+        while (sc.hasNext()) {
+            try {
+                sum += sc.nextInt();
+                index++;
+            } catch (InputMismatchException ignored) {
+            }
+        }
+        System.out.println(sum);
+        System.out.println(sum / index);
+
+    }
 }
