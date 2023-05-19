46fabc4dace31c148c5c999b1d22ccbe772c1c3a
STARTRACEX <startraceebr@foxmail.com>
2023-05-19 13:20:29 +0800

    update

--- src/writestream.java
+++ src/writestream.java
@@ -20,25 +20,25 @@ public class writestream {
             System.out.println("exception");
         }
 
-// //      不會構成亂碼
-// //       FileOutputStream
-//        try {
-//            File f = new File("hanz.txt");
-//            OutputStream out = new FileOutputStream(f);
-//            byte[] b = s.toString().getBytes();
-//            out.write(b);
-//            out.close();
-//        } catch (IOException e) {
-//            System.out.println("exception");
-//        }
-// //       FileWriter
-//        try {
-//            File f = new File("hanz.txt");
-//            Writer out = new FileWriter(f);
-//            out.write(s.toString());
-//            out.close();
-//        } catch (IOException e) {
-//            System.out.println("exception");
-//        }
+ //      不會構成亂碼
+ //       FileOutputStream
+        try {
+            File f = new File("hanz.txt");
+            OutputStream out = new FileOutputStream(f);
+            byte[] b = s.toString().getBytes();
+            out.write(b);
+            out.close();
+        } catch (IOException e) {
+            System.out.println("exception");
+        }
+ //       FileWriter
+        try {
+            File f = new File("hanz.txt");
+            Writer out = new FileWriter(f);
+            out.write(s.toString());
+            out.close();
+        } catch (IOException e) {
+            System.out.println("exception");
+        }
     }
 }
